f652cb0b147207a5008a2b0b3f2ff2d788a74422
MELLO <elimassaqui@gmail.com>
2025-06-05 13:03:24 +0100

    currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Plataform>().point; //define o ponto de spawn da primeira plataforma, no codigo Plataform.cs

--- Assets/Scripts/Spawn Plataform.cs
+++ Assets/Scripts/Spawn Plataform.cs
@@ -5,31 +5,36 @@ using UnityEngine;
 public class SpawnPlataform : MonoBehaviour
 {
 
-    public List<GameObject> plataforms = new List<GameObject>(); //lista de plataformas
-
+    public List<GameObject> plataforms = new List<GameObject>(); //lista de plataformas, de prefabs, a serem instanciadas
+    public List<Transform> currentPlatforms = new List<Transform>(); //lista de pontos de spawn, de objectos, onde as plataformas serao instanciadas
     public int offset; //distancia entre as plataformas
+
+    private Transform player;
+    private Transform currentPlatformPoint;
+    private int platformIndex; //indice da plataforma atual
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform; //encontra o jogador na cena
+
         // Instancia as plataformas na cena
         for (int i = 0; i < plataforms.Count; i++) //loop para instanciar as plataformas
         {
-            Instantiate(plataforms[i], new Vector3(0, 0, i * 22), transform.rotation); //instancia a plataforma na posicao 0,0,0
+            Transform p = Instantiate(plataforms[i], new Vector3(0, 0, i * 22), transform.rotation).transform; //instancia a plataforma na posicao 0,0,0
+            currentPlatforms.Add(p); //adiciona a plataforma instanciada na lista de plataformas
             offset += 22; //define a distancia entre as plataformas
         }
 
+        currentPlatformPoint = currentPlatforms[platformIndex].GetComponent<Plataform>().point; //define o ponto de spawn da primeira plataforma, no codigo Plataform.cs
     }
 
-    public GameObject myPlatform; //plataforma a ser reciclada
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R)) //se a tecla espaco for pressionada
-        {
-            recicle(myPlatform); //chama o metodo recicle
-        }
+        float distance = player.position.z - currentPlatformPoint.position.z; //calcula a distancia entre o jogador e o ponto de spawn da plataforma atual
+        Debug.Log("Distance: " + distance);
     }
 
     public void recicle(GameObject platform)
